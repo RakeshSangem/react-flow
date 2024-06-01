bb14842813d512a4477dccf5db4553a15d8cbf70
Rakesh Sangem <rakeshsangem88@gmail.com>
2024-06-01 14:30:15 +0530

    update next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: "export",
+//   distDir: "./dist",
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Outputs a Single-Page Application (SPA).
-  distDir: "./dist", // Changes the build output directory to `./dist/`.
+  distDir: "build", // Relative to the .next directory
 };
 
 export default nextConfig;
